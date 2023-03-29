7362d18220cb5e79f5f1d9624c19d48b63d6607c
pc04 <pc04@pc04>
2023-03-29 12:28:37 +0900

    20230329

--- JavaStudy/src/JANGYUJEONG/submit03/submit03.java
+++ JavaStudy/src/JANGYUJEONG/submit03/submit03.java
@@ -51,7 +51,53 @@ public class submit03 {
 			
 		}
 		
-
+		System.out.println("\n=============풀이============\n");
+		
+		//Q01. 10!값을 구하기(for문 사용)
+		// 10! = 10 x 9 x 8 x .... x 1
+		int rst = 1;
+		for(int i = 10; i >=1; i--) {
+			rst *= i;
+		}
+		System.out.println(rst);
+		
+		// @ 문제 15!
+		
+		long lrst = 1; // l 안붙여도됨
+		for(int i = 1; i <= 15; i++) {
+			lrst *= i;
+		}
+		System.out.println(lrst);
+		
+		// Q02. 월리가 몇개인지 찾기 
+		int count0 = 0;
+		String findWally0 = "윌리울리일리울리울리일리월리일리윌리월리울리일리일리월리일리윌리일리윌리일리월리월리윌리울리윌리울리일리울리울리윌리일리";
+		for(int i = 0; i < findWally0.length() -1; i++) {
+			
+			System.out.print(i + ":");
+			System.out.println(findWally0.substring(i,i+2));
+			String str = findWally0.substring(i,i+2);
+			
+			if(str.equals("월리")) {
+				count++;
+			}
+		}
+		
+		System.out.println("월리의 갯수: " + count);
+		
+		
+		
+		// Q03. 거꾸로 트리
+		// i가   0,1,2,3,4
+		// stars 5,4,3,2,1
+		// stars.substring(?) 의 결과
+		String stars0 = "*****";
+		for(int i =0; i<5; i++) {
+			System.out.println(stars.substring(i));
+		}
+		
+		
+		
 	}
 
 	
